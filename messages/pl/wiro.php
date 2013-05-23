44ef8967cd8d88603182c6fdff25d76757c5ab25
wirus <wirus15@gmail.com>
2013-05-23 15:44:09 +0200

    dodany plika messages, dodany BooleanColumn

---
+++ messages/pl/wiro.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'Yes' => 'Tak',
+    'No' => 'Nie',
+);
---
+++ widgets/grid/BooleanColumn.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace wiro\widgets\grid;
+
+use CDataColumn;
+
+/**
+ * @author Maciej Krawczyk <wirus15@gmail.com>
+ */
+class BooleanColumn extends CDataColumn 
+{
+    public $type = 'boolean';
+    
+    public function init()
+    {
+	parent::init();
+	if(!$this->filter)
+	    $this->fiter = array(
+		Yii::t('wiro', 'No'),
+		Yii::t('wiro', 'Yes'),
+	    );
+    }
+}
