68a1809f3469ce78c2beb66a9f8ae02b900f9eff
wirus15 <wirus15@gmail.com>
2013-06-10 17:54:18 +0200

    Revert "dodany szablon tytulu dla modulu podstron"
    This reverts commit eaca5a3ae5b44f92036091703baee0899729b5c5.

--- modules/pages/actions/ViewAction.php
+++ modules/pages/actions/ViewAction.php
@@ -3,25 +3,17 @@
 namespace wiro\modules\pages\actions;
 
 use CAction;
-use wiro\helpers\DefaultAttributes;
 use Yii;
 
 class ViewAction extends CAction 
 {
     public $defaultView = 'pages.views.front.view';
-    public $pageTitleTemplate;
     
     public function run($id)
     {
-	DefaultAttributes::set($this, array(
-	    'pageTitleTemplate' => Yii::app()->name.' - {pageTitle}',
-	));
-	
 	$model = Yii::app()->getModule('pages')->loadPage($id);
-	if($model->pageTitle) {
-	    $pageTitle = str_replace('{pageTitle}', $model->pageTitle, $this->pageTitleTemplate);
-	    $this->controller->pageTitle = $pageTitle;
-	}
+	if($model->pageTitle)
+	    $this->controller->pageTitle = $model->pageTitle;
 	$view = $model->pageView ? $model->pageView : $this->defaultView;
 	
 	$this->controller->render($view, array(
