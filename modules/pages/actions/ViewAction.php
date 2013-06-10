eaca5a3ae5b44f92036091703baee0899729b5c5
wirus15 <wirus15@gmail.com>
2013-06-10 17:11:26 +0200

    dodany szablon tytulu dla modulu podstron

--- modules/pages/actions/ViewAction.php
+++ modules/pages/actions/ViewAction.php
@@ -3,17 +3,25 @@
 namespace wiro\modules\pages\actions;
 
 use CAction;
+use wiro\helpers\DefaultAttributes;
 use Yii;
 
 class ViewAction extends CAction 
 {
     public $defaultView = 'pages.views.front.view';
+    public $pageTitleTemplate;
     
     public function run($id)
     {
+	DefaultAttributes::set($this, array(
+	    'pageTitleTemplate' => Yii::app()->name.' - {pageTitle}',
+	));
+	
 	$model = Yii::app()->getModule('pages')->loadPage($id);
-	if($model->pageTitle)
-	    $this->controller->pageTitle = $model->pageTitle;
+	if($model->pageTitle) {
+	    $pageTitle = str_replace('{pageTitle}', $model->pageTitle, $this->pageTitleTemplate);
+	    $this->controller->pageTitle = $pageTitle;
+	}
 	$view = $model->pageView ? $model->pageView : $this->defaultView;
 	
 	$this->controller->render($view, array(
