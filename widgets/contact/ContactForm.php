cfd2dd7d89bcf3f7224ed5848e41171cbaada7b8
wirus <wirus15@gmail.com>
2013-06-26 11:31:32 +0200

    commit

--- widgets/contact/ContactForm.php
+++ widgets/contact/ContactForm.php
@@ -4,8 +4,8 @@ namespace wiro\widgets\contact;
 
 use CMap;
 use CWidget;
-use TbHtml;
 use wiro\components\mail\YiiMailMessage;
+use wiro\helpers\DefaultAttributes;
 use Yii;
 
 /**
@@ -16,20 +16,60 @@ use Yii;
 class ContactForm extends CWidget
 {
     const TOKEN_VAR = 'form-token';
+    const VIEW_DEFAULT = 'default';
+    const VIEW_BOOTSTRAP = 'bootstrap';
     
     public $emailAddress;
     public $modelClass = 'wiro\widgets\contact\ContactFormModel';
-    public $view = 'default';
-    public $captchaOptions = array();
+    public $formClass;
+    public $formOptions = array();
+    public $view = self::VIEW_DEFAULT;
     public $useCaptcha = false;
+    public $captchaOptions = array();
     public $ajaxSubmit = false;
     private $model;
     private $messageSent = false;
+    private $form;
+    
+    public function __get($name)
+    {
+	try {
+	    return parent::__get($name);
+	} catch (CException $e) {
+	    return $this->form->__get($name);
+	}
+    }
+    
+    public function __set($name, $value)
+    {
+	try {
+	    return parent::__set($name, $value);
+	} catch (CException $e) {
+	    return $this->form->__set($name, $value);
+	}
+    }
+    
+    public function __isset($name)
+    {
+	try {
+	    return parent::__isset($name);
+	} catch (CException $e) {
+	    return $this->form->__isset($name);
+	}
+    }
+    
+    public function __unset($name)
+    {
+	try {
+	    return parent::__unset($name);
+	} catch (CException $e) {
+	    return $this->form->__unset($name);
+	}
+    }
     
     public function init()
     {
 	$this->model = Yii::createComponent($this->modelClass);
-	$this->emailAddress = Yii::app()->params->contactEmail;
 	$this->captchaOptions = CMap::mergeArray(array(
 		    'captchaAction' => '/site/captcha',
 		    'clickableImage' => true,
@@ -39,9 +79,16 @@ class ContactForm extends CWidget
 		    ),
 		), $this->captchaOptions);
 	$this->model->captchaAction = $this->captchaOptions['captchaAction'];
+	
+	DefaultAttributes::set($this, array(
+	    'emailAddress' => Yii::app()->params->contactEmail,
+	));
+	
+	$this->processSubmittedForm();
+	$this->renderFormHeader();
     }
-
-    public function run()
+    
+    private function processSubmittedForm()
     {
 	$modelClass = str_replace('\\', '_', $this->modelClass);
 	if(isset($_POST[$modelClass])) {
@@ -51,14 +98,27 @@ class ContactForm extends CWidget
 		$this->messageSent = true;
 	    }
 	}
-
-	$this->model->token = $this->generateToken();
+    }
+    
+    private function renderFormHeader()
+    {
 	echo '<div id="'.$this->id.'">';
+	$this->form = $this->beginWidget($this->formClass, $this->formOptions);
+	echo $this->form->hiddenField($this->model, 'token');
 	if($this->ajaxSubmit)
 	    echo $this->ajaxSubmitScript();
-	$this->render($this->view, array(
-	    'model' => $this->model,
-	));
+    }
+
+    public function run()
+    {
+	if($this->view !== null) {
+	    $this->render($this->view, array(
+		'form' => $this->form,
+		'model' => $this->model,
+	    ));
+	}
+	$this->model->token = $this->generateToken();
+	$this->endWidget();
 	echo '</div>';
     }
 
@@ -76,6 +136,11 @@ class ContactForm extends CWidget
     public function getModel()
     {
 	return $this->model;
+    }
+    
+    public function getForm()
+    {
+	return $this->form;
     }
 
     public function getMessageSent()
