e706fc9d5eb890102447db6c691ddf208e0058fb
Maciej Krawczyk <wirus15@gmail.com>
2013-10-27 11:41:50 +0100

    dodana funkcja before render do view action

--- actions/ViewAction.php
+++ actions/ViewAction.php
@@ -12,14 +12,26 @@ class ViewAction extends Action
      * @var string
      */
     public $view = 'view';
+    
+    /**
+     *
+     * @var Closure
+     */
+    public $beforeRender;
 
     /**
      * @param integer $id the ID of the model to be displayed
      */
     public function run($id)
     {
+        $model = $this->loadModel($id);
+        
+        if($this->beforeRender) {
+            $this->runClosure($this->beforeRender, $model);
+        }
+        
         $this->controller->render($this->view, array(
-            'model' => $this->loadModel($id),
+            'model' => $model,
         ));
     }
 
